03c3c790ca2c81de8cd0aa0f56bf227f6f5f3c10
Long <julyzergcn1@gmail.com>
2013-04-03 23:37:52 +0800

    set_language

--- yuce/settings.py
+++ yuce/settings.py
@@ -42,7 +42,12 @@ TIME_ZONE = 'Asia/Shanghai'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'zh-CN'
+LANGUAGE_CODE = 'zh-cn'
+gettext_noop = lambda s: s
+LANGUAGES = (
+    ('en', gettext_noop('English')),
+    ('zh-cn', gettext_noop('Simplified Chinese')),
+)
 
 SITE_ID = 1
 
@@ -90,8 +95,18 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    # Uncomment the next line for simple clickjacking protection:
-    # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
+)
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.contrib.auth.context_processors.auth',
+    'django.core.context_processors.debug',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.media',
+    'django.core.context_processors.static',
+    'django.core.context_processors.tz',
+    'django.core.context_processors.request',
+    'django.contrib.messages.context_processors.messages',
 )
 
 ROOT_URLCONF = 'yuce.urls'
--- yuce/urls.py
+++ yuce/urls.py
@@ -11,8 +11,8 @@ urlpatterns = patterns('',
     url(r'^accounts/profile/$', RedirectView.as_view(url='/')),
     url(r'^$', 'core.views.index', name='home'),
     url(r'^about/$', TemplateView.as_view(template_name='about.html'), name='about'),
-    url(r'contact/$', TemplateView.as_view(template_name='contact.html'), name='contact'),
-    
+    url(r'^contact/$', TemplateView.as_view(template_name='contact.html'), name='contact'),
+    url(r'^i18n/', include('django.conf.urls.i18n')),
 )
 
 from django.conf import settings as s
