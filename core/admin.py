1538b3a8fa0788919265266ff0b26ddde6a6ff7b
no <a@a.cc>
2013-04-08 17:11:51 +0800

    models

--- core/admin.py
+++ core/admin.py
@@ -1,13 +1,25 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as AuthUserAdmin
 
-from core.models import User
+from core.models import User, Action, Activity, Status, Tag, Topic, Bet
 from core.forms import UserCreationForm, UserChangeForm
 
 
 class UserAdmin(AuthUserAdmin):
-    list_display = AuthUserAdmin.list_display + ('last_login_ip',)
+    list_display = AuthUserAdmin.list_display + ('last_login_ip', 'coins')
     form = UserChangeForm
     add_form = UserCreationForm
 
 admin.site.register(User, UserAdmin)
+
+admin.site.register(Action,
+    list_display = ('short_text', 'text')
+)
+
+admin.site.register(Activity)
+admin.site.register(Status)
+admin.site.register(Tag)
+admin.site.register(Topic,
+    list_display = ('subject', 'status', 'created_date', 'deadline', 'close_date')
+)
+admin.site.register(Bet)
--- core/models.py
+++ core/models.py
@@ -2,6 +2,9 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.signals import user_logged_in
 from django.utils.translation import ugettext_lazy as _
+from django.utils import timezone
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes import generic
 
 
 def update_last_login_ip(sender, request, user, **kwargs):
@@ -16,4 +19,72 @@ user_logged_in.connect(update_last_login_ip)
 
 class User(AbstractUser):
     last_login_ip = models.CharField(_('last login ip'), max_length=20)
+    coins = models.PositiveIntegerField(default=0)
 
+class Action(models.Model):
+    short_text = models.CharField(max_length=20)
+    text = models.CharField(max_length=200)
+    
+    def __unicode__(self):
+        return self.short_text
+
+class Activity(models.Model):
+    user = models.ForeignKey(User)
+    action = models.ForeignKey(Action)
+    created_date = models.DateTimeField(default=timezone.now)
+    content_type = models.ForeignKey(ContentType, null=True, blank=True)
+    object_id = models.PositiveIntegerField(null=True, blank=True)
+    target = generic.GenericForeignKey('content_type', 'object_id')
+    text = models.CharField(max_length=200, blank=True)
+    
+    class Meta:
+        verbose_name_plural = _('Activities')
+    
+    def __unicode__(self):
+        return u'[%s]%s %s %s'%(self.date, self.user, self.action.text, self.target or self.text)
+
+###################
+
+class Status(models.Model):
+    short_text = models.CharField(max_length=20)
+    text = models.CharField(max_length=20)
+    
+    class Meta:
+        verbose_name_plural = _('Statuses')
+    
+    def __unicode__(self):
+        return self.text
+
+class Tag(models.Model):
+    tag = models.CharField(max_length=200)
+    
+    def __unicode__(self):
+        return self.tag
+
+class Topic(models.Model):
+    subject = models.CharField(max_length=200)
+    subject_english = models.CharField(max_length=200, blank=True)
+    content = models.TextField()
+    content_english = models.TextField(blank=True)
+    status = models.ForeignKey(Status, default=1)
+    tags = models.ManyToManyField(Tag, null=True, blank=True)
+    created_date = models.DateTimeField(default=timezone.now)
+    deadline = models.DateTimeField()
+    close_date = models.DateTimeField()
+    end_weight = models.PositiveIntegerField()
+    
+    def __unicode__(self):
+        return self.subject
+
+class Bet(models.Model):
+    user = models.ForeignKey(User)
+    topic = models.ForeignKey(Topic)
+    coins = models.PositiveIntegerField()
+    weight = models.PositiveIntegerField()
+    yesno = models.BooleanField()
+    created_date = models.DateTimeField(default=timezone.now)
+    status = models.ForeignKey(Status)
+    status_date = models.DateTimeField(default=timezone.now)
+    
+    def __unicode__(self):
+        return u'%s %s'%(self.user, self.topic)
