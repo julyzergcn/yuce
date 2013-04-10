3104a83e4e491b5175a6737b401924fca595b634
no <a@a.cc>
2013-04-10 18:05:21 +0800

    new topic

--- core/forms.py
+++ core/forms.py
@@ -2,6 +2,8 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm as AuthUserCreationForm
 from django.contrib.auth.forms import UserChangeForm as AuthUserChangeForm
 from django.utils.translation import ugettext_lazy as _
+from django.contrib.admin.widgets import AdminSplitDateTime
+from django.forms.extras.widgets import SelectDateWidget
 
 from core.models import User
 
@@ -21,3 +23,14 @@ class UserCreationForm(AuthUserCreationForm):
         except User.DoesNotExist:
             return username
         raise forms.ValidationError(self.error_messages['duplicate_username'])
+
+class TopicCreationForm(forms.Form):
+    subject = forms.CharField()
+    subject_english = forms.CharField(required=False)
+    content = forms.CharField(widget=forms.Textarea)
+    content_english = forms.CharField(widget=forms.Textarea, required=False)
+    deadline = forms.DateTimeField(widget=AdminSplitDateTime)
+    end_date = forms.DateTimeField(widget=AdminSplitDateTime)
+    yes = forms.BooleanField(required=False)
+    coins = forms.IntegerField(min_value=0, required=False)
+    
--- core/views.py
+++ core/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django import http
 
+from core.models import *
+from core.forms import *
+
 
 def index(request):
     return render(request, 'index.html')
@@ -9,7 +12,17 @@ def topics(request):
     return render(request, 'core/topics.html')
 
 def new_topic(request):
-    return render(request, 'core/new_topic.html')
+    if request.method == 'POST':
+        form = TopicCreationForm(request.POST)
+        if form.is_valid():
+            print form.cleaned_data
+    else:
+        form = TopicCreationForm()
+    context = {
+        'form': form,
+        'tags': Tag.objects.all(),
+    }
+    return render(request, 'core/new_topic.html', context)
 
 def archived_topics(request):
     return render(request, 'core/archived_topics.html')
